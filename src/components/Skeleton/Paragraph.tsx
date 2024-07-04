dd12cde3a1496252831339123d85293f2e933c7d
SongInJae <pan0663@naver.com>
2024-07-04 23:38:21 +0900

    feat: Skeleton Paragraph 추가

---
+++ src/components/Skeleton/Paragraph.tsx
@@ -0,0 +1,17 @@
+import Box from "./Box";
+
+interface ParagraphProps {
+  line: number;
+}
+
+const Paragraph = ({ line, ...props }: ParagraphProps) => {
+  return (
+    <div {...props}>
+      {Array.from(Array(line), (_, index) => {
+        return <Box width="100%" height={16} key={index} />;
+      })}
+    </div>
+  );
+};
+
+export default Paragraph;
